76c717b8929c133aa1d446ff337ef53cf204c117
kuruczmisi <kurucz.misi@icloud.com>
2026-07-09 10:24:01 +0200

    Add H2 test profile

--- src/test/java/org/example/f1database/controller/TeamControllerIntegrationTest.java
+++ src/test/java/org/example/f1database/controller/TeamControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
+import org.springframework.test.context.ActiveProfiles;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class TeamControllerIntegrationTest {
 
     @Autowired
